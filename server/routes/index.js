9fc9a79f1351a569b5a1b3ca5240bef964027f29
Gear61 <chessnone@yahoo.com>
2019-09-19 00:28:31 -0700

    JWT demo

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,9 @@ var fs = require('fs');
 
 module.exports = function(app) {
 	fs.readdirSync(__dirname).forEach(function(file) {
-		if (file == "index.js") return;
+		if (file == "index.js" || file == "keys" || file == "auth_helper.js") {
+			return;
+		}
 		var name = file.substr(0, file.indexOf('.'));
 		require('./' + name)(app);
 	});
