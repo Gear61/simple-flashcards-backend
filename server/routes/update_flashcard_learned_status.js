bc86dd5331e215710993dc7e37bb9842b9486977
Gear61 <chessnone@yahoo.com>
2019-09-29 22:27:37 -0700

    Endpoint to update learned status

---
+++ server/routes/update_flashcard_learned_status.js
@@ -0,0 +1,45 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: true
+});
+const authHelper = require(require('path').resolve(__dirname, './auth_helper.js'));
+
+module.exports = function(app) {
+	app.post('/flashcard/update_learned_status', async (request, response) => {
+		try {
+			var authToken = request.header('auth_token');
+			var expandedToken = authHelper.verify(authToken);
+			if (!expandedToken) {
+				response.status(401);
+				response.send();
+				return;
+			}
+			
+			var requestBody = request.body;
+			var flashcardId = requestBody['flashcard_id'];
+			var learned = requestBody['learned'];
+
+			const updateQuery = 'UPDATE Flashcard SET learned = $1 WHERE id = $2'
+			const values = [learned, flashcardId];
+
+			const client = await pool.connect();
+			client.query(updateQuery, values)
+			.then(res => {
+				response.status(200);
+				response.send();
+			})
+			.catch(exception => {
+				console.error(exception.stack)
+				response.status(500);
+				response.send();
+			});
+
+			client.release();
+		} catch (exception) {
+			console.error(exception.stack)
+			response.status(500);
+			response.send();
+		}
+	});
+}
