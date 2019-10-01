2e07a34b3ff88367d8f443320552ded3e2b923b0
Gear61 <chessnone@yahoo.com>
2019-09-30 22:23:03 -0700

    Add flashcard sets to folder endpoint

---
+++ server/routes/add_flashcard_sets_into_folder.js
@@ -0,0 +1,50 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: true
+});
+const authHelper = require(require('path').resolve(__dirname, './auth_helper.js'));
+
+module.exports = function(app) {
+	app.post('/folder/add_sets', async (request, response) => {
+		try {
+			var authToken = request.header('auth_token');
+			var expandedToken = authHelper.verify(authToken);
+			if (!expandedToken) {
+				response.status(401);
+				response.send();
+				return;
+			}
+
+			var requestBody = request.body;
+			var folderId = requestBody['id'];
+			var setIds = requestBody['flashcard_set_ids'];
+
+			const client = await pool.connect();
+			try {
+				await client.query('BEGIN');
+
+				for (var i = 0; i < setIds.length; i++) {
+					const insertConnectionQuery =
+					'INSERT INTO FlashcardSetInFolder(folder_id, flashcard_set_id)' +
+					' VALUES($1, $2)';
+					const insertConnectionsValues = [folderId, setIds[i]];
+					await client.query(insertConnectionQuery, insertConnectionsValues);
+				}
+
+				await client.query('COMMIT');
+				response.status(200);
+				response.send();
+			} catch (e) {
+				await client.query('ROLLBACK')
+				throw e
+			} finally {
+				client.release()
+			}
+		} catch (exception) {
+			console.error(exception.stack)
+			response.status(500);
+			response.send(error);
+		}
+	});
+}
