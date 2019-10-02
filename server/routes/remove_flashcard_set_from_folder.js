2e362f0443350860bcfef36cff09c3c698054de0
Gear61 <chessnone@yahoo.com>
2019-10-01 20:52:46 -0700

    Remove flashcard set from folder endpoint

---
+++ server/routes/remove_flashcard_set_from_folder.js
@@ -0,0 +1,47 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: true
+});
+const authHelper = require(require('path').resolve(__dirname, './auth_helper.js'));
+
+module.exports = function(app) {
+	app.post('/folder/remove_set', async (request, response) => {
+		try {
+			var authToken = request.header('auth_token');
+			var expandedToken = authHelper.verify(authToken);
+			if (!expandedToken) {
+				response.status(401);
+				response.send();
+				return;
+			}
+
+			var requestBody = request.body;
+			var folderId = requestBody['folder_id'];
+			var setId = requestBody['flashcard_set_id'];
+
+			const client = await pool.connect();
+			try {
+				await client.query('BEGIN');
+
+				const deleteConnectionQuery =
+				'DELETE FROM FlashcardSetInFolder WHERE folder_id = $1 and flashcard_set_id = $2';
+				const deleteConnectionsValues = [folderId, setId];
+				await client.query(deleteConnectionQuery, deleteConnectionsValues);
+
+				await client.query('COMMIT');
+				response.status(200);
+				response.send();
+			} catch (e) {
+				await client.query('ROLLBACK')
+				throw e
+			} finally {
+				client.release()
+			}
+		} catch (exception) {
+			console.error(exception.stack)
+			response.status(500);
+			response.send(error);
+		}
+	});
+}
