157500445c047f3da41e04f055b24f84760a028f
Gear61 <chessnone@yahoo.com>
2019-10-06 18:12:35 -0700

    Cleaner email onboarding flow

--- server/routes/login_with_email.js
+++ server/routes/login_with_email.js
@@ -7,92 +7,89 @@ const pool = new Pool({
 const authHelper  = require(require('path').resolve(__dirname, './auth_helper.js'));
 
 module.exports = function(app) {
-	app.post('/onboarding/email', function(request, response) {
-		if (!('email' in request.body)) {
-			response.status(401);
-			response.send({error: 'Email address missing from payload'});
-			return;
-		}
-
-		const email = request.body['email'];
-		const name = request.body['name'];
-		const profilePictureUrl = request.body['profile_picture_url'];
-		const loginType = request.body['login_type'];
-
-		signUp(name, email, profilePictureUrl, loginType, response);
-	});
-}
-
-async function signUp(name, email, profilePictureUrl, loginType, response) {
-	const accountQuery = 'SELECT id, name, email, profile_picture_url, login_type '
-	+ 'FROM Account WHERE email = $1';
-	const values = [email];
-
-	const client = await pool.connect();
-	client.query(accountQuery, values)
-	.then(res => {
-		client.end();
-		// Account doesn't exist yet; create it and return
-		if (res.rows.length == 0) {
-			console.log("Account doesn't exist for email: " + email);
-			createAccount(name, email, profilePictureUrl, loginType, response);
-		} else {
-			if (res.rows[0].login_type == loginType) {
-				console.log("Account already created with " + loginType);
-				var payload = {
-					user_id: res.rows[0].id
-				}
-				var authToken = authHelper.sign(payload);
-				console.log("Auth token: " + authToken);
-				var responseJson = {
-					auth_token: authToken,
-					name: res.rows[0].name,
-					email: res.rows[0].email,
-					profile_picture_url: res.rows[0].profile_picture_url,
-					new_account: false
-				}
-
-				response.send(responseJson);
-			} else {
-				console.log("Email already used, can't create with " + loginType);
+	app.post('/onboarding/email', async (request, response) => {
+		try {
+			if (!('email' in request.body)) {
 				response.status(401);
-				const error = {'error': 'email_in_use'};
-				response.send(error);
+				response.send({error: 'Email address missing from payload'});
+				return;
 			}
-		}
-	})
-	.catch(exception => {
-		console.error(exception.stack)
-		response.status(500);
-		response.send();
-	});
-}
 
-async function createAccount(name, email, profilePictureUrl, loginType, response) {
-	const insert_query = 'INSERT INTO Account(name, email, profile_picture_url, login_type) ' +
-	'VALUES($1, $2, $3, $4) RETURNING id'
-	const values = [name, email, profilePictureUrl, loginType];
+			const email = request.body['email'];
+			const name = request.body['name'];
+			const profilePictureUrl = request.body['profile_picture_url'];
+			const loginType = request.body['login_type'];
 
-	const client = await pool.connect();
-	client.query(insert_query, values)
-	.then(res => {
-		var payload = {
-			user_id: res.rows[0].id
-		}
-		var authToken = authHelper.sign(payload);
-		var responseJson = {
-			auth_token: authToken,
-			name: name,
-			email: email,
-			profile_picture_url: profilePictureUrl,
-			new_account: true
-		}
+			const client = await pool.connect();
+			try {
+				await client.query('BEGIN');
 
-		response.send(responseJson);
-	})
-	.catch(exception => {
-		console.error(exception.stack)
-		response.status(500);
-		response.send();
+				var statusCode = 200;
+				var responseJson = {};
+
+				const findUserQuery = 'SELECT id, name, email, profile_picture_url, login_type '
+				+ 'FROM Account WHERE email = $1';
+				const findUserValues = [email];
+
+				const results = await client.query(findUserQuery, findUserValues);
+				if (results.rows.length > 0) {
+					// Account already exists, do the login flow
+					if (results.rows[0].login_type == loginType) {
+						console.log("Account already created with " + loginType + " for email: " + email);
+						var payload = {
+							user_id: results.rows[0].id
+						}
+						var authToken = authHelper.sign(payload);
+						console.log("Auth token: " + authToken);
+						responseJson = {
+							auth_token: authToken,
+							name: results.rows[0].name,
+							email: results.rows[0].email,
+							profile_picture_url: results.rows[0].profile_picture_url,
+							new_account: false
+						}
+					}
+					// Email already used for a different login type, reject
+					else {
+						console.log("Email " + email + " already used, can't create with " + loginType);
+						statusCode = 401;
+						responseJson = {'error': 'email_in_use'};
+					}
+				}
+				// Account doesn't exist, sign up flow
+				else {
+					const createAccountQuery = 'INSERT INTO Account(name, email, profile_picture_url, ' +
+						'login_type) VALUES($1, $2, $3, $4) RETURNING id'
+					const createAccountValues = [name, email, profilePictureUrl, loginType];
+					const createResult = await client.query(createAccountQuery, createAccountValues);
+
+					var payload = {
+						user_id: createResult.rows[0].id
+					}
+					var authToken = authHelper.sign(payload);
+					responseJson = {
+						auth_token: authToken,
+						name: name,
+						email: email,
+						profile_picture_url: profilePictureUrl,
+						new_account: true
+					}
+				}
+
+				response.status(statusCode);
+				response.send(responseJson);
+
+				await client.query('COMMIT');
+			} catch (e) {
+				await client.query('ROLLBACK')
+				throw e
+			} finally {
+				client.release()
+			}
+		} catch (exception) {
+			console.error(exception.stack)
+			response.status(500);
+			response.send();
+		}
 	});
 }
