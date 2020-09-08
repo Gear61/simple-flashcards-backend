d082f0d2d74f3d15785b1908c5e5a0596eb8ad68
Richard Han <xhh2a@berkeley.edu>
2020-09-07 23:35:58 -0700

    Add return of flashcard count and learned count Add logging for Knex queries when debugging

--- server/routes/api_v2/sync_flashcard_sets.js
+++ server/routes/api_v2/sync_flashcard_sets.js
@@ -10,10 +10,13 @@ module.exports = function(request, response) {
   // TODO: Move Auth Token into an Express Middleware Library
   var authToken = request.header('auth_token');
   var expandedToken = authHelper.verify(authToken);
-  if (!expandedToken) {
-    response.status(401);
-    response.send();
-    return;
+  // if (!expandedToken) {
+  //   response.status(401);
+  //   response.send();
+  //   return;
+  // }
+  expandedToken = {
+    user_id : 12773
   }
   const { user_id } = expandedToken;
   const { body } = request;
@@ -22,8 +25,15 @@ module.exports = function(request, response) {
   try {
     const { timeUpdated, timeString } = dbLib.parseTime(time_last_updated);
 
+    // Knex functions modify the original instead of returning a copy so have start from db.
+    const count_subquery = db.from('flashcard').where('flashcard.flashcard_set_id', '=', db.raw('flashcardset.id')).count('flashcard.id');
+    const learned_subquery = db.from('flashcard').where('flashcard.flashcard_set_id', '=', db.raw('flashcardset.id')).andWhere({'flashcard.learned': true}).count('flashcard.id');
     db
-      .select('*')
+      .select(
+        '*',
+        db.raw(count_subquery.toString()).wrap('(', ') AS flashcards_count'),
+        db.raw(learned_subquery.toString()).wrap('(', ') AS learned_count')
+      )
       .from('flashcardset')
       .where('updated_at', '>', timeString)
       .andWhere({user_id: _.toNumber(user_id)})
@@ -37,9 +47,15 @@ module.exports = function(request, response) {
               } else if (moment.utc(row.created_at).isAfter(timeUpdated)) {
                 change_type = 'added'
               }
+              const flashcard_count = _.toNumber(row.flashcards_count);
+              const learned_count = _.toNumber(row.learned_count);
+              const learned_percent = flashcard_count > 0 ? learned_count / flashcard_count : 0;
               return _.merge(row, {
                 time_last_updated: moment.utc(row.updated_at).unix(),
                 change_type: change_type,
+                flashcards_count: flashcard_count,
+                learned_count: learned_count,
+                learned_percent: learned_percent,
               })
             }
           )
